b8851f3f5391d4a9a0aa3b495527f2d1437e3904
fqhrmp <fqhrmp@gmail.com>
2021-09-19 23:03:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def default():
-    return render_template("index.html")
+    return "hello piki"
 
 @app.route('/predict', methods=['POST'])
 def predict():
